82e0bd7459d72df77f7c08182c461debbf5b06ab
badach02 <dxb718@psu.edu>
2026-05-18 16:42:01 -0400

    health

--- app.py
+++ app.py
@@ -45,6 +45,9 @@ def top300():
         "players": players
     })
 
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
